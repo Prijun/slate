66d9fd524888670829172a3df5209ef9bfff8d92
Primora Studios Slate <prijun@gmail.com>
2026-09-15 10:58:05 +0200

    project structure

---
+++ api/plans.js
@@ -0,0 +1,13 @@
+const supabase = require('../lib/supabaseClient');
+
+module.exports = async function handler(req, res) {
+  if (req.method === 'GET') {
+    return res.status(200).json({ plans: [] });
+  }
+
+  if (req.method === 'POST') {
+    return res.status(501).json({ message: 'not implemented yet' });
+  }
+
+  return res.status(405).json({ message: 'method not allowed' });
+};
